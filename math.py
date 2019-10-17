ad12c21cebb1ec8c10044ad62d67c710ac2286e8
Allard <AllardQuek@users.noreply.github.com>
2019-10-17 11:44:05 +0800

    fixed add function

--- math.py
+++ math.py
@@ -1,7 +1,7 @@
 def add(num1, num2):
     if isinstance(num1, str) or isinstance(num2, str):
         return False
-    # return num1 + num2
+    return num1 + num2
 
 def multiply(n1, n2):
     result = n1 * n2
